086534bab6972e6079b68dd5eba5be27ff18dbf7
a-abdelmageed <a.abdelmajeedali@gmail.com>
2023-04-22 15:46:49 -0700

    Edited

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,16 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
-int get_precision(const char *format, int *i, va_list list)
+* print_percent - Print %
+* @vlist: arguments passed to print
+* @output_p: Host output
+* @o_p: output position
+*
+* Description: Fuction that print %
+* Return: the int
+*/
+int print_percent(va_list vlist, char *output_p, int o_p)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
-
-	if (format[curr_i] != '.')
-		return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+(void) vlist;
+output_p[o_p] = '%';
+return (++o_p);
 }
